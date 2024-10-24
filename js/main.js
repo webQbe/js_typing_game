66a8d0279b13c883bdbda65ddc135564a388f7ef
webQbe <webcubesolutionslk@gmail.com>
2024-10-24 18:38:09 +0530

    Defined DOM Elements

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ let score = 0;
 let isPlaying; // Is game going on?
 
 
+// Define DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+
